3ff9931d57b27750b10b22502a497cc7b594f067
bennettdevelop <bennettdevelop@gmail.com>
2018-08-09 19:40:58 +0300

    part of tests

--- features/utility/initialization.py
+++ features/utility/initialization.py
@@ -3,7 +3,6 @@ from features.pages.aP_login_page import ApLoginPage
 from features.pages.general_page import GeneralPage
 from features.pages.enter_tt_page import EnterTTPage
 from features.pages.lock_tt_page import LockTTPage
-from features.pages.reports_page import ReportsPage
 
 class Pages:
     at_login_page = AtLoginPage()
@@ -11,4 +10,3 @@ class Pages:
     general_page = GeneralPage()
     enter_tt_page = EnterTTPage()
     lock_tt_page = LockTTPage()
-    reports_page = ReportsPage()
