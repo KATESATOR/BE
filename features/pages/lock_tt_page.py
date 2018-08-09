cdf3bc061b9e5a2becb1666b225943ae83f85c34
bennettdevelop <bennettdevelop@gmail.com>
2018-08-09 19:40:58 +0300

    part of tests

--- features/pages/lock_tt_page.py
+++ features/pages/lock_tt_page.py
@@ -4,32 +4,38 @@ from features.utility.element import *
 class LockTTPage:
 
     def __init__(self):
-        self.user_barber_robert = Button("//*[@title='Barber, Robert']", "xpath", "Barber, Robert user")
-
-        self.lock_tt_button = Link("Lock Time-Track", "link text", "lock tt page")
+        self.lock_tt_button = Link("Lock Time-Track", "link text", "tt_page")
         self.date_selector_button = Button("ext-gen32", "id", "date selector")
-        self.user_selector_button = Button("usersSelector", "id", "user selector")
-        self.all_staff_radibutton = Button("//*[@class='x-form-cb-label'][contains(text(), 'All Staff')]",
-                                           "xpath", "all staff radiobutton")
-        self.selected_users_radiobutton = Button("//*[@class='x-form-cb-label'][contains(text(), 'Selected users')]",
-                                                 "xpath", "selected users radiobutton")
-        self.close_user_selector_button = Button("//*/span[contains(text(), 'Close')]",
-                                                 "xpath", "Close users selector")
-        self.clear_filter_button = Button("clearButton", "id", "Clear user filter")
         self.current_week_button = Button("//*[@class='rangeItemTitle'][contains(text(), 'Current week')]",
                                           "xpath", "Current week")
+        self.cur_and_prev_week_button = Button(
+            "//*[@class='rangeItemTitle'][contains(text(), 'Current & previous weeks')]",
+            "xpath", "Current & previous weeks")
+        self.users_selector_button = Button("usersSelector", "id", "users selector")
+        self.all_staff_button = Button("//*[@class='x-form-cb-label'][contains(text(), 'All Staff')]",
+                                       "xpath", "selected users radio button")
+        self.selected_users_button = Button("//*[@class='x-form-cb-label'][contains(text(), 'Selected users')]",
+                                            "xpath", "selected users radio button")
+        self.user_barber_robert = Button("//*[@title='Barber, Robert']", "xpath", "Barber, Robert user")
+        self.close_users_selector_button = Button("//*/span[contains(text(), 'Close')]",
+                                                  "xpath", "Close users selector")
+        self.clear_filter_button = Button("clearButton", "id", "Clear user filter")
+
         self.task_name = Button("//*[@class='lockTableEllipsis'][contains(text(), 'Backup controlling')]",
                                 "xpath", "task")
+
         self.type_of_work_button = Button("//*[@class='typeOfWorkCell']/div[2]/div",
                                           "xpath", "Type of work")
         self.type_programming = Button("//*[@class='typeOfWorkRow']/div[contains(text(), 'programming')]",
                                        "xpath", "programming type")
+
         self.type_non_billable = Button("//*[@class='typeOfWorkRow']/div[contains(text(), 'Non-Billable')]",
                                         "xpath", "Non-Billable type")
+
         self.date_cell = Button("//*[@class='dateCellNonWorking    ']", "xpath", "today")
-        self.user_date_cell = Button("//*[@class='userDayCell    ']", "xpath", "one user day")
-        self.lock_selected = Link("Lock Selected", "link text", "lock selected")
-        self.unlock_selected = Link("Unlock Selected", "link text", "unlock selected")
+        self.lock_selected = Link("Lock Selected", "link text", "Lock selected")
+        self.unlock_selected = Link("Unlock Selected", "link text", "Unlock selected")
+
         # userTasksHeaderCell
         self.expand_all_button = Button("//*[@onclick='expandAllUserRows()']", "xpath", "expand all")
         self.collapse_all_button = Button("//*[@onclick='collapseAllUserRows()']", "xpath", "collapse all")
