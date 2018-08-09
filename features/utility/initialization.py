3ac865e0454dcbf549ec0b8facbd7d98aca451f8
bennettdevelop <bennettdevelop@gmail.com>
2018-08-09 19:40:58 +0300

    part of tests
    (PRO-1334)Refactoring and complete part of smoke tests on Lock TT

--- features/utility/initialization.py
+++ features/utility/initialization.py
@@ -2,10 +2,11 @@ from features.pages.aT_login_page import AtLoginPage
 from features.pages.aP_login_page import ApLoginPage
 from features.pages.general_page import GeneralPage
 from features.pages.enter_tt_page import EnterTTPage
-
+from features.pages.lock_tt_page import LockTTPage
 
 class Pages:
     at_login_page = AtLoginPage()
     ap_login_page = ApLoginPage()
     general_page = GeneralPage()
     enter_tt_page = EnterTTPage()
+    lock_tt_page = LockTTPage()
