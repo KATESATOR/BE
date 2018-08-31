2aa9358055320fabff03386dd4b51ccdd18ed044
bennettdevelop <bennettdevelop@gmail.com>
2018-08-31 11:17:59 +0300

    (PRO-1334) Refactoring and complete part of smoke tests on Lock TT (PRO-1353) Create, save and export reports as Admin

--- features/pages/lock_tt_page.py
+++ features/pages/lock_tt_page.py
@@ -4,37 +4,39 @@ from features.utility.element import *
 class LockTTPage:
 
     def __init__(self):
-        self.lock_tt_button = Link("Lock Time-Track", "link text", "tt_page")
+        self.user_barber_robert = Button("//*[@title='Barber, Robert']", "xpath", "Barber, Robert user")
+
+        self.lock_tt_button = Link("Lock Time-Track", "link text", "lock tt page")
+        self.result_text = TextField("//*[@class='result-number']", "xpath", "result number")
         self.date_selector_button = Button("ext-gen32", "id", "date selector")
+        self.user_selector_text = Button("ext-gen44", "id", "user selector text")
+        self.user_selector_button = Button("usersSelector", "id", "user selector")
+        self.all_staff_radibutton = Button("//*[@class='x-form-cb-label'][contains(text(), 'All Staff')]",
+                                           "xpath", "all staff radiobutton")
+        self.selected_users_radiobutton = Button("//*[@class='x-form-cb-label'][contains(text(), 'Selected users')]",
+                                                 "xpath", "selected users radiobutton")
+        self.close_user_selector_button = Button("//*/span[contains(text(), 'Close')]",
+                                                 "xpath", "Close users selector")
+        self.clear_filter_button = Button("clearButton", "id", "Clear user filter")
         self.current_week_button = Button("//*[@class='rangeItemTitle'][contains(text(), 'Current week')]",
                                           "xpath", "Current week")
-        self.cur_and_prev_week_button = Button(
-            "//*[@class='rangeItemTitle'][contains(text(), 'Current & previous weeks')]",
-            "xpath", "Current & previous weeks")
-        self.users_selector_button = Button("usersSelector", "id", "users selector")
-        self.all_staff_button = Button("//*[@class='x-form-cb-label'][contains(text(), 'All Staff')]",
-                                       "xpath", "selected users radio button")
-        self.selected_users_button = Button("//*[@class='x-form-cb-label'][contains(text(), 'Selected users')]",
-                                            "xpath", "selected users radio button")
-        self.user_barber_robert = Button("//*[@title='Barber, Robert']", "xpath", "Barber, Robert user")
-        self.close_users_selector_button = Button("//*/span[contains(text(), 'Close')]",
-                                                  "xpath", "Close users selector")
-        self.clear_filter_button = Button("clearButton", "id", "Clear user filter")
-
-        self.task_name = Button("//*[@class='lockTableEllipsis'][contains(text(), 'Backup controlling')]",
-                                "xpath", "task")
-
-        self.type_of_work_button = Button("//*[@class='typeOfWorkCell']/div[2]/div",
-                                          "xpath", "Type of work")
-        self.type_programming = Button("//*[@class='typeOfWorkRow']/div[contains(text(), 'programming')]",
-                                       "xpath", "programming type")
-
-        self.type_non_billable = Button("//*[@class='typeOfWorkRow']/div[contains(text(), 'Non-Billable')]",
-                                        "xpath", "Non-Billable type")
+        self.task_backup_controlling = Button("//*[@class='lockTableEllipsis'][contains(text(), 'Backup controlling')]",
+                                "xpath", "task Backup controlling")
 
         self.date_cell = Button("//*[@class='dateCellNonWorking    ']", "xpath", "today")
-        self.lock_selected = Link("Lock Selected", "link text", "Lock selected")
-        self.unlock_selected = Link("Unlock Selected", "link text", "Unlock selected")
+        self.user_date_cell = Button("//*[@class='userDayCell    ']", "xpath", "one user day")
+        self.lock_selected = Link("Lock Selected", "link text", "lock selected")
+        self.unlock_selected = Link("Unlock Selected", "link text", "unlock selected")
+
+        # Types of Work
+        self.type_of_work_button = Button("//*[@class='typeOfWorkButton editable']",
+                                          "xpath", "Type of work")
+        self.type_programming = Button("//*[@class='typeLabel ellipsis'][contains(text(), 'programming')]",
+                                       "xpath", "programming type")
+        self.type_non_billable = Button("//*[@class='typeLabel ellipsis'][contains(text(), 'Non-Billable')]",
+                                        "xpath", "Non-Billable type")
+
+
 
         # userTasksHeaderCell
         self.expand_all_button = Button("//*[@onclick='expandAllUserRows()']", "xpath", "expand all")
