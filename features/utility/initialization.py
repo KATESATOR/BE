5ceaf18aca471c73b79ba6272e95a31e255cc8f4
mikhaylov <alexey.mikhaylov@actimind.com>
2018-10-02 10:17:39 +0300

    Completed tests for User list by admin and by user

--- features/utility/initialization.py
+++ features/utility/initialization.py
@@ -2,7 +2,8 @@ from features.pages.aT_login_page import AtLoginPage
 from features.pages.aP_login_page import ApLoginPage
 from features.pages.general_page import GeneralPage
 from features.pages.enter_tt_page import EnterTTPage
-from features.pages.tasks_page import TasksPage
+from features.pages.approve_tt_page import ApproveTTPage
+from features.pages.user_list_page import UserListPage
 
 
 class Pages:
@@ -10,4 +11,5 @@ class Pages:
     ap_login_page = ApLoginPage()
     general_page = GeneralPage()
     enter_tt_page = EnterTTPage()
-    tasks_page = TasksPage()
+    approve_tt_page = ApproveTTPage()
+    user_list_page = UserListPage()
