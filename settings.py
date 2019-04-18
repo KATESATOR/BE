e355e49b2dfe883fd031554ca3da2fa17c481f1e
bennettdevelop <bennettdevelop@gmail.com>
2019-04-18 15:01:31 +0300

    added config and refactoring logged steps

--- settings.py
+++ settings.py
@@ -2,3 +2,4 @@ import os
 
 FEATURES_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'features')
 LOGS_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'features/results/logs')
+USER_ROLES_CONFIG_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'features/utility/user_roles.cfg')
